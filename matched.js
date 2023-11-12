6d9bde44d2605197bae5f38466c90538f87f8d88
mdhamimhowladerasif <mdhamimhowladerasif@gmail.com>
2023-11-13 02:01:08 +0600

    first commit

---
+++ matched.js
@@ -0,0 +1,10 @@
+const numbers = [12, 23, 52, 61, 36];
+
+// for (let i = 0; i < number.length; i++) {
+//     const element = number[i];
+//     console.log(element);
+// }
+
+for(const number of numbers){
+    console.log(number);
+}
---
+++ object.js
@@ -0,0 +1,9 @@
+const nayok = {
+    name: 'sakib khan',
+    id: 24 ,
+    address:'dhaka asuliea',
+    isSingle:false,
+    
+
+
+}
